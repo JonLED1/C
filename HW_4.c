25e79f4035535303529fad8521994df85256729f
JonLED1 <122430375+JonLED1@users.noreply.github.com>
2024-02-03 13:29:35 +0300

    Add files via upload

---
+++ HW_4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf(" __    __\n");
+	printf("/  \\__/  \\\n");
+	printf("\\__/  \\__/\n");
+	printf("/  \\__/  \\\n");
+	printf("\\__/  \\__/\n");
+	return 0;
+}
+
