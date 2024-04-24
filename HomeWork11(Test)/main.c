bbd293fd0421885566b78659e0f6b013c8042df9
Jon LED <jonyl1977@gmail.com>
2024-04-24 09:37:24 +0300

    add check month

--- HomeWork11(Test)/main.c
+++ HomeWork11(Test)/main.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include "temp_functions.h"
 
-
-
 int main(int argc, char *argv[]){
 
     //no command
@@ -15,29 +13,55 @@ int main(int argc, char *argv[]){
 
     //command parametrs
     FILE *file_in;
-    char file_name[256];
+    char *file_name;
+    int month=0;
     int rez=0;
     opterr=0;
+    struct sensor_temp {
+        short int day;
+        short int month;
+        short int year;
+        short int hour;
+        short int min;
+        short int t;
+    };
+
     while ( (rez = getopt(argc,argv,"hf:m:")) != -1){
         switch (rez){
-            case 'h': print_help(); break;
-            case '?': print_help_small(); break;
-            case 'f': strncpy(file_name, optarg, sizeof(file_name)); break;
-            case 'm': printf("Month = %s\n", optarg); break;
+            case 'h': print_help(); return 0;
+            case '?': print_help_small(); return 0;
+            case 'f': file_name=optarg; break;
+            case 'm': month=atoi(optarg); break;
         };
 
     };
 
     file_in=fopen(file_name, "r");
+    //check file
     if (!file_in){
-        printf("File name - %s not found!\n", file_name);
+        printf("File name - <%s> not found!\n", file_name);
+        fclose(file_in);
         return 0;
     }
     else{
-        printf("File name - %s opened.\n", file_in);
+        printf("File name - <%s> opened.\n", file_name);
     };
 
-    printf("Run program\n");
+    //check month
+    if (month<0 || month>12){
+        printf("Invalid month!");
+        fclose(file_in);
+        return 0;
+    }
+
+    struct sensor_temp *date_in;
+    
+    if (month==0){
+        stat_all();
+    }
+    else{
+        stat_month();
+    }
     fclose(file_in);
 
     return 0;
--- HomeWork11(Test)/temp_functions.c
+++ HomeWork11(Test)/temp_functions.c
@@ -2,8 +2,8 @@ void print_help(void){
     printf ("\n\nProgramm for calculate average temperature.\n");
     printf ("Please, use this keys for command line:\n");
     printf ("\t-h Help\n");
-    printf ("\t-f File_name\n");
-    printf ("\t-m Month\n\n");
+    printf ("\t-f <file_name.csv>\n");
+    printf ("\t-m Month (1-12)\n\n");
 }
 
 void print_help_small(void){
